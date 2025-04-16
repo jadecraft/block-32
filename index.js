1813919b7dbc82cd9168d3e60b72df610d6fb26d
Jade Craft <jadecraft9@gmail.com>
2025-04-16 19:58:16 -0400

    update

--- index.js
+++ index.js
@@ -12,33 +12,33 @@ const client = new pg.Client(
     await client.connect();
 
     const SQL =`
-    DROP TABLE IF EXSISTS flavors;
+    DROP TABLE IF EXISTS flavors;
     CREATE TABLE flavors(
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    is_favorite BOOLEAN NOT NULL,
+    is_favorite BOOLEAN DEFAULT false,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
     INSERT INTO flavors(name, is_favorite) VALUES ('vanilla', false);
     INSERT INTO flavors(name, is_favorite) VALUES ('cookies n cream', true);
     INSERT INTO flavors(name, is_favorite) VALUES ('chocolate', false);
     INSERT INTO flavors(name, is_favorite) VALUES ('smores', true);
-    INSERT INTO flavors(name, is_favorite) VALUES ('rocky road, false);
+    INSERT INTO flavors(name, is_favorite) VALUES ('rocky road', false);
     `;
 
     await client.query(SQL);
     console.log("data seeded");
 
     const port = process.env.PORT || 3000;
-    server.listen(port, () => console.log('listening on port ${port}'));
+    server.listen(port, () => console.log(`listening on port ${port}`));
 
     };
 
     server.get("/api/flavors", async (req, res, next) => {
         try {
             const SQL = `
-            SELECT * from flavors:
+            SELECT * FROM flavors;
             `;
     
             const response = await client.query(SQL);
@@ -48,6 +48,72 @@ const client = new pg.Client(
                 next(error);
             };
         });
+
+    server.get("/api/flavors/:id", async (req, res, next) => {
+        const {id} = req.params
+        try {
+            const SQL = `
+            SELECT * FROM flavors
+            WHERE id=$1`
+
+            const response = await client.query(SQL, [id])
+
+            res.send(response.rows)
+            
+        } catch (error) {
+            console.log(error)
+        }
+    });
+
+    server.post('/api/flavors', async (req, res, next ) => {
+        const {name} = req.body
+        try {
+          const SQL = `
+          INSERT INTO flavors (name)
+          VALUES ($1)
+          RETURNING *`  
+          
+          const response = await client.query(SQL, [name])
+          res.json(response.rows)
+
+        }   catch (error) {
+            console.log (error)
+        }
+    });
+
+    server.delete("/api/flavors/:id", async (req, res, next) => {
+         const {id} = req.params
+         try {
+            const SQL = `
+            DELETE FROM flavors
+            WHERE id=$1`
+            await client.query(SQL,[id])
+            res.send("deleted")
+
+         } catch (error) {
+            console.log(error)
+         }
+    });
+
+    server.put('/api/flavors/:id', async (req, res, next ) => {
+        const {name} = req.body
+        const {id}= req.params
+        try {
+          const SQL = `
+          UPDATE flavors
+          SET name=$1
+          WHERE id=$2
+          RETURNING *`
+          const response = await client.query(SQL, [name, id])
+          res.json(response.rows)
+
+        }   catch (error) {
+            console.log (error)
+        }
+    });
+
+        
+
     
     // init function invocation
     init();
